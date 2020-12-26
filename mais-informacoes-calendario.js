ab95fc59c7928ed5b826258cd865b81fe9f7ddde
Filipe <filipe@Filipes-MacBook.local>
2020-12-26 21:44:39 -0200

    Pre-estruturando scripts

---
+++ mais-informacoes-calendario.js
@@ -0,0 +1,8 @@
+const dataCalendario1 = new Date(/* Data 1 */);
+const dataCalendario2 = new Date(/* Data 2 */);
+const dataCalendario3 = new Date(/* Data 3 */);
+const dataCalendario4 = new Date(/* Data 4 */);
+const dataCalendario5 = new Date(/* Data 5 */);
+const dataCalendario6 = new Date(/* Data 6 */);
+const dataCalendario7 = new Date(/* Data 7 */);
+const dataCalendario8 = new Date(/* Data 8 */);
