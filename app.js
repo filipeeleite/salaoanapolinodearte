b3e59d836f1845c33f965d8024fe86079eda5312
filipeeleite <fls2007.filipe@gmail.com>
2021-02-12 00:39:53 -0200

    v3-pre1 structuring

---
+++ app.js
@@ -0,0 +1,4 @@
+var link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("href", "/css/footer.css");
+document.head.appendChild(link);
---
+++ objects.js
@@ -0,0 +1,59 @@
+edicao23 = [
+    artista = 'blue',
+]
+
+
+edicao24 = [
+    artistas = {
+        [ // Nome do artista - tag - classe - / onclick para pagina
+            ['Adriana Vignoli - DF', 'p', 'nome']
+            ['Andréa Hygino - RJ', 'p', 'nome']
+            ['Claudio Trindade - SC', 'p', 'nome']
+            ['Cristiana Nogueira - AP', 'p', 'nome']
+            ['Danielle Fonseca - PA', 'p', 'nome']
+            ['Daré - SP', 'p', 'nome']
+            ['Élcio Miazaki - SP', 'p', 'nome']
+            ['Eriel Araújo - BA', 'p', 'nome']
+            ['Estefânia Young - RS', 'p', 'nome premiado']
+            ['Fernanda Adamski - GO', 'p', 'nome']
+            ['class="premiado">Fernanda Azou - DF', 'p', 'nome']
+            ['class="premiado nome-longo">Guerreiro do Divino Amor - RJ', 'p', 'nome']
+            ['Hariel Revignet - GO', 'nome']
+            ['class="premiado">Joardo Filho - GO', 'p', 'nome']
+            ['Júlia Milward - SP', 'p', 'nome']
+            ['Mano Penalva - SP', 'p', 'nome']
+            ['Marcelo Amorim - SP', 'p', 'nome']
+            ['Maurício Pokemon - PI', 'p', 'nome']
+            ['Renato Rios - SP', 'p', 'nome']
+            ['Yara Pina - GO', 'p', 'nome']
+        ]
+    }
+
+]
+
+
+
+<h3>Artistas:</h3>
+</div>
+<ol class="grid-ed-ant-lista-1">
+    <li>Adriana Vignoli - DF</li>
+    <li>Andréa Hygino - RJ</li>
+    <li>Claudio Trindade - SC</li>
+    <li>Cristiana Nogueira - AP</li>
+    <li>Danielle Fonseca - PA</li>
+    <li>Daré - SP</li>
+    <li>Élcio Miazaki - SP</li>
+    <li>Eriel Araújo - BA</li>
+    <li class="premiado">Estefânia Young - RS</li>
+    <li>Fernanda Adamski - GO</li>
+    <li class="premiado">Fernanda Azou - DF</li>
+    <li class="premiado nome-longo">Guerreiro do Divino Amor - RJ</li>
+    <li>Hariel Revignet - GO</li>
+    <li class="premiado">Joardo Filho - GO</li>
+    <li>Júlia Milward - SP</li>
+    <li>Mano Penalva - SP</li>
+    <li>Marcelo Amorim - SP</li>
+    <li>Maurício Pokemon - PI</li>
+    <li>Renato Rios - SP</li>
+    <li>Yara Pina - GO</li>
+</ol>
